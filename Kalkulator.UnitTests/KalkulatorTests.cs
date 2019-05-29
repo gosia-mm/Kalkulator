8aa35f1c7c015fb93c0eef77e2f375ffe36a075f
gosia-mm <malgosia.mrula@gmail.com>
2019-05-29 17:57:18 +0200

    Utworzenie projektu

---
+++ Kalkulator.UnitTests/KalkulatorTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+using Kalkulator.Kalkulator;
+
+namespace Kalkulator.UnitTests
+{
+    [TestFixture]
+    public class KalkulatorTests
+    {
+        [Test]
+        public void Dodawanie_CzyPoprawnyWynik(double liczba1, double liczba2, double poprawnyWynik)
+        {
+            var kalkulator = new Kalkulator.Program();
+            wynikTestowanegoProgramu = Program
+            Assert.That(Is.EqualTo(poprawnyWynik))
+        }
+    }
+}
---
+++ Kalkulator/Kalkulator/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulator
+{
+    class Program
+    {
+        public static double Dodawanie(double liczba1, double liczba2)
+        {
+            return liczba1 + liczba2;
+        }
+
+        public static double Odejmowanie(double liczba1, double liczba2)
+        {
+            return liczba1 - liczba2;
+        }
+
+        public static double Mnozenie(double liczba1, double liczba2)
+        {
+            return liczba1 * liczba2;
+        }
+
+        public static double Dzielenie(double liczba1, double liczba2)
+        {
+            if (liczba2 != 0)
+                return liczba1 / liczba2;
+            else
+            {
+                Console.WriteLine("Nie dziel przez 0!");
+                return 0;
+            }
+        }
+
+        public static double WykonanieDzialania(double liczba1, double liczba2, string dzialanie)
+        {
+            double wynik = 0;
+
+            switch (dzialanie)
+            {
+                case "Dodawanie":
+                    wynik = Dodawanie(liczba1, liczba2);
+                    break;
+                case "Odejmowanie":
+                    wynik = Odejmowanie(liczba1, liczba2);
+                    break;
+                case "Mnozenie":
+                    wynik = Mnozenie(liczba1, liczba2);
+                    break;
+                case "Dzielenie":
+                    wynik = Dzielenie(liczba1, liczba2);
+                    break;
+                default:
+                    Console.WriteLine("Nie wybrano działania");
+                    break;
+            }
+
+            return wynik;
+        }
+
+        static void Main(string[] args)
+        {
+            double liczba1 = 0, liczba2 = 0;
+            string dzialanie = "";
+
+            double wynik = WykonanieDzialania(liczba1, liczba2, dzialanie);
+
+        }
+    }
+}
